5f0c997fd7b007e8fbd76163ecf39a6cbf2e525e
Doxygen CI <ci-arduino@invalid>
2024-08-04 18:11:08 +0000

    Deploy docs to GitHub Pages from commit fe4585a417
    Commit: fe4585a417587b62b4453c818f985bb29284fcc2
    GitHub Actions run: 10238272988

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['selectchannel',['selectChannel',['../class_adafruit___d_s248x.html#a96734f4c5e1472f831e388bc5b0003d5',1,'Adafruit_DS248x']]],
   ['shortdetected',['shortDetected',['../class_adafruit___d_s248x.html#af2dded9684cfb0fe35fde8734d554760',1,'Adafruit_DS248x']]],
   ['singlebitresult',['singleBitResult',['../class_adafruit___d_s248x.html#a7203f28db0cbbd36e59590957e711779',1,'Adafruit_DS248x']]],
   ['strongpullup',['strongPullup',['../class_adafruit___d_s248x.html#ab3a29da1fc1c3935bb9dded33f886e30',1,'Adafruit_DS248x']]]
